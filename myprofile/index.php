d5bb4b8fcf030e53b901afee53e5be7715b78887
nickrazie <32930267+nickrazie@users.noreply.github.com>
2018-06-14 23:33:51 +0300

    Add files via upload

---
+++ myprofile/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>
+		My Profile
+	</title>
+</head>
+<body>
+
+	<link rel="stylesheet" href="css.css">
+	<div class="split left">
+		<div class="centered">
+			<img src="mypic.jpg" alt="Avatar woman">
+			<h2>Nicholas Rotich</h2>
+			<p>Bsc. Computer Forensics and Security</p>
+		</div>
+	</div>
+
+	<div class="split right">
+	</div>
+
+</body>
+</html>
